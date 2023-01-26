76944fc9af79bf98f763fa27c54a981db21a3677
Ernesto Cardenas <71235388+Ernestocardenas@users.noreply.github.com>
2023-01-25 23:26:27 -0500

    lab 12 dataviz upload

--- js/app.js
+++ js/app.js
@@ -1,117 +1,182 @@
 'use strict';
 
-let totalVotes = 25
-let allProducts = []
+// delcaring a products array
+let productsArr = [];
+// console.log(productsArr);
 
-function Product(name, photoExt = 'jpg') {
+// creating a queue (for later use)
+let uniqueProducts = [];
+
+// setting a counter to keep track of number of clicks
+let clickCounter = 0;
+
+let results = document.getElementById('result');
+
+// Assigning HTML img elements to 3 separate image variables, will be manipulated to add src and alt attributes
+let image1 = document.querySelector('section img:first-child');
+let image2 = document.querySelector('section img:nth-child(2)');
+let image3 = document.querySelector('section img:last-child');
+
+// creating a contructor named Products with various properties
+function Product(name, src) {
   this.name = name;
-  this.photo = 'img/$(name).$(photoExt)';
-  this.view = 0
-  this.votes= 0
-
-  allProducts.push(this);
+  this.src = src;
+  this.viewed = 0;
+  this.clicked = 0;
+  productsArr.push(this);
 }
 
-Product.allProducts = []
+// creating new products with the Products constructor
+let product1 = new Product ('R2D2 Bags', './img/bag.jpg');
+let product2 = new Product ('Banana Cutter', './img/banana.jpg');
+let product3 = new Product ('Tablet TP', './img/bathroom.jpg');
+let product4 = new Product ('Breakfast Machine', './img/breakfast.jpg');
+let product5 = new Product ('Meatball BG', './img/bubblegum.jpg');
+let product6 = new Product ('Uncomfortable Chair', './img/chair.jpg');
+let product7 = new Product ('Cthulhu', '.iImg/cthulhu.jpg');
+let product8 = new Product ('Dog Duck', './img/dog-duck.jpg');
+let product9 = new Product ('Dragon Meat', './img/dragon.jpg');
+let product10 = new Product ('Weird Pens', './img/pen.jpg');
+let product11 = new Product ('Pet Sweep', './img/pet-sweep.jpg');
+let product12 = new Product ('Pizza Cutter', './img/scissors.jpg');
+let product13 = new Product ('Shark Bed', './img/shark.jpg');
+let product14 = new Product ('Baby Sweep', './img/sweep.png');
+let product15 = new Product ('Tauntaun', './img/tauntaun.jpg');
+let product16 = new Product ('Unicorn Meat', './img/unicorn.jpg');
+let product17 = new Product ('Water Can', './img/water-can.jpg');
+let product18 = new Product ('Wine Glass', './img/wine-glass.jpg');
+let product19 = new Product ('Useless Boots', './img/boots.jpg');
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * allProducts.length)
+
+// -----------------------------------
+// 2. Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
+// -----------------------------------
+
+
+// creating a function to generate a random index from the productsArr
+function generateRandomProdIndex () {
+  return Math.floor(Math.random() * productsArr.length);
 }
 
-let testInt1 = 0;
-let testInt2 = 0;
-let testInt3 = 0;
 
-function randomProduct() {
-  let imgOneIndex = getRandomNumber();
-  let imgTwoIndex = getRandomNumber();
-  let imgThreeIndex = getRandomNumber();
+function renderProductImages () {
 
-  while ((imgOneIndex === imgTwoIndex || imgOneIndex === imgThreeIndex || imgTwoIndex === imgThreeIndex || imgOneIndex === testInt1 || imgOneIndex === testInt2 || imgOneIndex === testInt3 || imgTwoIndex === testInt1 || imgTwoIndex === testInt2 || imgTwoIndex === testInt3 || imgThreeIndex === testInt1 || imgThreeIndex === testInt2 || imgThreeIndex === testInt3));
-    imgOneIndex = getRandomNumber();
-    imgTwoIndex = getRandomNumber();
-    imgThreeIndex = getRandomNumber();
+  while (uniqueProducts.length < 6 ) {
 
+    let randomIndex = generateRandomProdIndex();
+    console.log(`The random product is: ${productsArr[randomIndex].name}`);
 
-imgOne.alt = allProducts[imgOneIndex].name;
-  imgOne.src = allProducts[imgOneIndex].photo;
-  allProducts[imgOneIndex].views++;
-  imgTwo.src = allProducts[imgTwoIndex].photo;
-  imgTwo.alt = allProducts[imgTwoIndex].name;
-  allProducts[imgTwoIndex].views++;
-  imgThree.alt = allProducts[imgThreeIndex].name;
-  imgThree.src = allProducts[imgThreeIndex].photo;
-  allProducts[imgOneIndex].views++;
-  testInt1 = imgOneIndex;
-  testInt2 = imgTwoIndex;
-  testInt3 = imgThreeIndex;
-}
-
-function handleClick(event) {
-  if (event.target === imgContainer) {
-    alert('Please click on an image');
-  }
-  totalVotes--;
-  let imgClicked = event.target.alt;
-  for (let i = 0; i < allProducts.length; i++) {
-    if (imgClicked === allProducts[i].name) {
-      allProducts[i].votes++;
-      break;
+    if(!uniqueProducts.includes(productsArr[randomIndex])) {
+      uniqueProducts.push(productsArr[randomIndex]);
+      console.log('the unique products are: ', uniqueProducts);
     }
   }
-  if (totalVotes === 0) {
-    renderChart();
-    imgContainer.removeEventListener('click', handleClick);
 
-    let stringedProducts = JSON.stringify(allProducts);
-    
-    console.log(stringedProducts); 'yah it works'
-  
-    localStorage.setItem('myThings', stringedProducts);
+  let firstImageObject = uniqueProducts.shift();
+  let secondImageObject = uniqueProducts.shift();
+  let thirdImageObject = uniqueProducts.shift();
 
-  } else {
-    renderProducts();
-  }
+  console.log('The three unique images are :', firstImageObject.name, secondImageObject.name, thirdImageObject.name);
+  console.log('The remain unique products after shifts is:', uniqueProducts);
+
+  image1.src = firstImageObject.src;
+  image1.alt = firstImageObject.name;
+  image1.title = firstImageObject.name;
+
+  image2.src = secondImageObject.src;
+  image2.alt = secondImageObject.name;
+  image2.title = secondImageObject.name;
+
+  image3.src = thirdImageObject.src;
+  image3.alt = thirdImageObject.name;
+  image3.title = thirdImageObject.name;
+
+  firstImageObject.viewed++;
+  secondImageObject.viewed++;
+  thirdImageObject.viewed++;
 }
 
-let gotProducts = localStorage.getItem('myProducts');
+// creating an event handler that records how many times each image is clicked. afterwards, increments the amount of times the image has been clicked and re-renders each image. Finally, an if statement to keep track of the click counter, once the click counter exceeds 25 clicks, the event listeners for each image is removed.
+function handleClickedProduct(event) {
+  clickCounter++;
+  let selection = event.target;
 
-let parsedProducts = JSON.parse(gotProducts);
-console.log(parsedProducts);
-
-if(gotProducts){
-  console.log('parsed products', parsedProducts);
-  allThings = parsedThings;
-
-  for (let i=0; i < parsedThings.length; i++){
-    if(parsedProducts[i].name === 'sweep'){
-      let reconProduct = new Thing(parsedProducts[i].name, 'png');
-      reconProduct.views = parsedProducts[i].views;
-      reconProduct.votes = parsedProducts[i].votes;
-    } else {
-      let reconProduct = new Thing(parsedProducts[i].name);
-      reconProduct.views = parsedProducts[i].views;
-      reconProduct.votes = parsedProducts[i].votes;
+  for (let i = 0; i < productsArr.length; i++) {
+    if (selection.alt.includes(productsArr[i].name)) {
+      productsArr[i].clicked++;
+      // console.log(productsArr[i]);
     }
   }
-} else {
-  new Product('bag');
-  new Product('banana');
-  new Product('bathroom');
-  new Product('boots');
-  new Product('breakfast');
-  new Product('bubblegum');
-  new Product('chair');
-  new Product('cthulhu');
-  new Product('dog-duck');
-  new Product('dragon');
-  new Product('pen');
-  new Product('pet-sweep');
-  new Product('scissors');
-  new Product('shark');
-  new Product('sweep', 'png');
-  new Product('tauntaun');
-  new Product('unicorn');
-  new Product('water-can');
-  new Product('wine-glass');
+
+  renderProductImages();
+
+  if (clickCounter > 24) {
+    image1.removeEventListener('click', handleClickedProduct);
+    image2.removeEventListener('click', handleClickedProduct);
+    image3.removeEventListener('click', handleClickedProduct);
+  }
 }
+
+// Event handler for button to view results of how many times each image has been viewed and clicked.
+function handleViewResults() {
+  let prodUL = document.querySelector('ul');
+
+  for (let i = 0; i < productsArr.length; i++ ) {
+    let prodLI = document.createElement('li');
+    prodLI.innerText = `${productsArr[i].name} was viewed ${productsArr[i].viewed} times and was clicked on ${productsArr[i].clicked} times!`;
+    prodUL.appendChild(prodLI);
+  }
+
+  // creating empty arrays for names, views and clicks
+  let names = [];
+  let views = [];
+  let clicks = [];
+
+  // for loop to iterate through each item in the productsArr array, each iteration pushes the current indices name, views, and clicks into their corresponding array.
+  for (let i = 0; i < productsArr.length; i++) {
+    names.push(productsArr[i].name);
+    views.push(productsArr[i].viewed);
+    clicks.push(productsArr[i].clicked);
+  }
+
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: 'Views',
+        data: views,
+        borderWidth: 1
+      },
+      {
+        label: 'Clicks',
+        data: clicks,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  if (clickCounter > 24) {
+    results.removeEventListener('click', handleViewResults);
+  }
+}
+
+// image render (on page load)
+renderProductImages();
+
+// event listeners
+image1.addEventListener('click', handleClickedProduct);
+image2.addEventListener('click', handleClickedProduct);
+image3.addEventListener('click', handleClickedProduct);
+results.addEventListener('click', handleViewResults);
+
+
